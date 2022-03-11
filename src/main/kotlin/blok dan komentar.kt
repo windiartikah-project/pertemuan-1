f7c0d1d5476fa528308d46fadfedb85e83f92867
windiartikah <windiartikah25@gmail.com>
2022-03-11 16:49:59 +0700

    percobaan var

---
+++ src/main/kotlin/blok dan komentar.kt
@@ -0,0 +1,3 @@
+fun main() {
+    println("hello ini program pertama saya ")
+}
---
+++ src/main/kotlin/variabel.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var dynamicVar = 2
+    println(" 3 = " + 4 )
+//berdasarkan compiler kotlin akan mengenali bahwa variabel dynamic var bertipe int
+    var staticVar : String = "A"
+    println (" 4 = " +  2 )
+//dapat di ketahui bahwa variabel staticVar memiliki tipe data Char.
+}
