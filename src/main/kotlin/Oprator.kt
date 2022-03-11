74a802f7d22174d40d0428cdefc95cd871d4cdf5
windiartikah <windiartikah25@gmail.com>
2022-03-11 16:20:06 +0700

    percobaan oprator

---
+++ src/main/kotlin/Oprator.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var a = 7
+    var b = 6
+    var c = (a++ + b++) / 2.0
+    println(c + b)
+
+    var dinamis = "hore"
+    var statis : Double = 2.0
+    println(statis)
+    statis = 3.0
+    statis = 3.14
+    val PHI = 3.14
+
+
+}
