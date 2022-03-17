a0566daf15865fd9258c3f317bd17b949aaedf69
windiartikah <windiartikah25@gmail.com>
2022-03-17 23:40:09 +0700

    blok dan komentar

--- src/main/kotlin/blok dan komentar.kt
+++ src/main/kotlin/blok dan komentar.kt
@@ -1,3 +1,14 @@
+//Blok
 fun main() {
-    println("hello ini program pertama saya ")
+    //blok ditandai dengan tanda buka dan tutup kurung kurawal
+    println("Hello, ini Program Pertama Saya di Kotlin!")
 }
+
+//Komentar
+// Contoh komentar 1 baris
+/*
+Contoh komentar 1 baris atau lebih
+ */
+/**
+ * dokumentasi kode program
+ */
