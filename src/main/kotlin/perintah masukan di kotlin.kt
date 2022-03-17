2f6aaea5a6858ee98182f9155115e990d486549b
windiartikah <windiartikah25@gmail.com>
2022-03-17 23:52:15 +0700

    perintah masukan di kotlin

---
+++ src/main/kotlin/perintah masukan di kotlin.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    //Parrot program
+    println("Hi I'm Lora, the virtual parrot, and i love to repeat!");
+    println("Type something in: ");
+    var input: String
+    input = readLine()!!
+    var output: String
+    output = input + ", " + input + "!"
+    println(output)
+}
+
+//Untuk merubah nilai String yang dikembalikan oleh fungsi readLine()!! kedalam nilai yang diinginkan
+//fun main(args: Array<String>) {
+//Doubler program parsing
+//    println("Enter a number and I'll double it: ")
+//   val input = readLine()!!
+//  var a = input.toInt() // eventually Double
+//   a = a * 2
+//   println(a)
+
+//    println("Enter a number, and I'll square it:")
+//    val number = readLine()!!.toInt()
+//   val square = number * number
+//   println("Result: " + square)
+//}
