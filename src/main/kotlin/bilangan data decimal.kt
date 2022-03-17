12010c26e9ee980b0be7803cef81ff2cef356e90
windiartikah <windiartikah25@gmail.com>
2022-03-18 01:04:53 +0700

    bilangan data decimal

---
+++ src/main/kotlin/bilangan data decimal.kt
@@ -0,0 +1,8 @@
+//Tipe data bilangan decimal
+fun main(args: Array<String>) {
+    val maxFloat: Float = 9.123456789f
+    val maxDouble: Double = 9.123456789
+
+    println("maxFloat:" + maxFloat)
+    println("maxDouble:" + maxDouble)
+}
