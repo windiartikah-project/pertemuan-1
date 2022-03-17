820fc4bbabb32c4cfea65cea3446d6c000d06943
windiartikah <windiartikah25@gmail.com>
2022-03-18 00:17:45 +0700

    percobaan boolean

---
+++ src/main/kotlin/percobaan boolean.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var varBool : Boolean = true
+    if(varBool)
+        println("varBool bernilai " + varBool)
+    var varInt : Int = 2
+    //Jika menggunakan if(varInt) maka program akan eror karena terdapat 2 kondisi if
+    // if(varInt)
+    println("varInt bernilai " + varInt)
+}
